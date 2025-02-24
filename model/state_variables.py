31131a43a63c3a85af413a0a9c6c7d6ddfe815d7
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-24 21:16:57 +0800

    start exp

--- model/state_variables.py
+++ model/state_variables.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass, field
 from typing import List
 
+
 @dataclass
 class StateVariables:
-    
+
     start: int = 0
 
-initial_state = StateVariables().__dict__
 
+initial_state = StateVariables().__dict__
--- model/system_parameters.py
+++ model/system_parameters.py
@@ -2,8 +2,10 @@ from dataclasses import dataclass
 from typing import List
 from model.utils import default
 
+
 @dataclass
 class Parameters:
-    start: List[int] = default([1,3,4])
+    start: List[int] = default([1, 3, 4])
+
 
 parameters = Parameters().__dict__
